ef504f87acc0d8e41fa4aafc7d1c8b4bd6a001ba
jhoisz <jvrda@ufpi.edu.br>
2022-07-28 22:32:15 -0300

    jhoisnayra#1

--- Main.java
+++ Main.java
@@ -1,11 +1,14 @@
 import java.util.Arrays;
 
 public class Main {
-
 	public static void main(String[] args) {
-		Menu mainMenu =  new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes"));
-		System.out.println(mainMenu.getSelection() + "foi selecionada");
+		Menu mainMenu =  new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes", "Sair"));
+
+		do {
+//			mainMenu =  new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes", "Sair"));
+			System.out.println(mainMenu.getSelection() + " foi selecionada");			
+		}while (mainMenu.getSelection() !=4);
+
 		System.out.println("Fim");
 	}
-
 }
