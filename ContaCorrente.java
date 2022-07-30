92a9838fb063bdd9b62aac7c193dfa03a1934f7f
Guimaand <andersonguima@ufpi.edu.br>
2022-07-29 22:48:13 -0300

    feat: adicionando conta poupanca

---
+++ ContaCorrente.java
@@ -0,0 +1,30 @@
+
+public class ContaCorrente extends Conta{
+    private float limite;
+    private float taxaManutencao;
+
+    @Override
+    public boolean sacar(float valor) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean transferir(Conta conta, float valor) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean depositar(float valor) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void exibirExtrato() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
