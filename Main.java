284b13a2c75e2689b8f4d22401d83fb24ad14782
Rubem Eslley <rubemeslley@gmail.com>
2022-07-30 09:17:41 -0300

    Eslley#12 (#6)
    * feat: adicionando classe Cliente#3
    
    * feat: adicionando classe abstrata Conta
    
    * feat: adicionando classe ContaPoupanca
    
    * feat: adicionando clientes#12

--- Main.java
+++ Main.java
@@ -1,11 +1,35 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
+	
+	public static ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+	
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
 		Menu mainMenu =  new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes", "Sair"));
 
 		do {
-//			mainMenu =  new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes", "Sair"));
+			switch (mainMenu.getSelection()) {
+			case 1:
+				
+				break;
+			case 2:
+				System.out.println("Digite o CPF do cliente:");
+				String cpf = scan.next();
+				
+				System.out.println("Digite o nome do cliente:");
+				String nome = scan.next();
+				
+				clientes.add(new Cliente(cpf, nome));
+				break;
+			case 3:
+				
+				break;
+			default:
+				break;
+			}
 			System.out.println(mainMenu.getSelection() + " foi selecionada");			
 		}while (mainMenu.getSelection() !=4);
 
