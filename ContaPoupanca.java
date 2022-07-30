bf239e74609d7ec2fc6ede1144f6bc2372cfcd69
Rubem Eslley <rubemeslley@gmail.com>
2022-07-29 21:59:59 -0300

    Eslley#5 (#4)
    * feat: adicionando classe Cliente#3
    
    * feat: adicionando classe abstrata Conta
    
    * feat: adicionando classe ContaPoupanca

---
+++ ContaPoupanca.java
@@ -0,0 +1,30 @@
+
+public class ContaPoupanca extends Conta {
+	
+	private float rendimento;
+
+	@Override
+	public boolean sacar(float valor) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean transferir(Conta conta, float valor) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean depositar(float valor) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void exibirExtrato() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
