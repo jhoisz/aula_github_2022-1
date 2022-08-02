f3dd6bc14ae844badc5c23160569d2b0832fec73
eluvya <eluvyapiauilino@gmail.com>
2022-08-01 21:16:43 -0300

    apaga arquivos

--- ContaPoupanca.java
+++ ContaPoupanca.java
@@ -12,22 +12,13 @@ public class ContaPoupanca extends Conta {
 
 	@Override
 	public boolean transferir(Conta conta, float valor) {
-		if (saldo > valor) {
-			saldo = saldo - valor;
-			conta.setSaldo(conta.getSaldo() + valor);
-			return true;
-		}
+		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean depositar(float valor) {
 		setSaldo(saldo + valor);
-		return true;
-	}
-
-	public boolean saldo(Conta conta) {
-		System.out.println("Seu saldo: " + conta.getSaldo());
 		return true;
 	}
 
